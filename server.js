e379a18a97d0f21758d4757bf012715b2b07edef
Felipe <felipe@gooseinsurance.com>
2024-06-05 18:25:49 -0700

    Refactored server process.env updated

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import bodyParser from "body-parser";
+import 'dotenv/config'
 
 import appRoutes from './routes/index.js'
 
 const app = express();
-const PORT = 3333;
-
 app.use(bodyParser.json());
 
 app.use('/', appRoutes);
 app.all("*", (req, res) => res.send("You've tried reaching a route that doesn't exist."));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
+
